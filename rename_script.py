208b92a3ff95e72d2a1b9d43cc947ab96190a72a
flcode17 <andrewserqeyev@gmail.com>
2025-01-12 21:05:42 -0500

    24 gallery html/css

--- rename_script.py
+++ rename_script.py
@@ -37,4 +37,4 @@ def rename_photos(folder_path):
         print(f"Error: {e}")
 
 # Example usage
-rename_photos(r"/Volumes/Macintosh HD/Andrew/Work/PycharmProjects/Python/Primavera Website/Events/006_Amore_More/Photos")
+rename_photos(r"/Volumes/Macintosh HD/Andrew/Work/PycharmProjects/Python/Primavera Website/Events/010_Preburn/Photos")
